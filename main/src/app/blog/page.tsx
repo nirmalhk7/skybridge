858f5474ba16b2ca93b7c1cf2852adbc2fe6fb36
Nirmal Khedkar <nirmalhk7@gmail.com>
2025-03-01 15:35:14 -0700

    Breadcrumb changes, more accurate
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- main/src/app/blog/page.tsx
+++ main/src/app/blog/page.tsx
@@ -16,6 +16,7 @@ const Blog = () => {
       <Breadcrumb
         pageName="Blog Grid"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
+        breadcrumbs={[{name:"Home",href:"/"},{name:"Blog Grid", href:"/blog"}]}
       />
 
       <section className="pb-[120px] pt-[120px]">
