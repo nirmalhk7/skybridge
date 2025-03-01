ca7a4bd19c2ae97de759b155ec6ae004ea0091bd
Nirmal Khedkar <nirmalhk7@gmail.com>
2025-03-01 15:36:47 -0700

    Fixed contact page error
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

--- main/src/app/contact/page.tsx
+++ main/src/app/contact/page.tsx
@@ -1,5 +1,5 @@
 import Breadcrumb from "@/components/Common/Breadcrumb";
-import Contact from "@/components/Contact";
+
 
 import { Metadata } from "next";
 
@@ -16,8 +16,6 @@ const ContactPage = () => {
         pageName="Contact Page"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
       />
-
-      <Contact />
     </>
   );
 };
