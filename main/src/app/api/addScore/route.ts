b15200ae2d692aa3053ae1fef01ada15628cfb35
pranay0302 <pka.pranayandra02@gmail.com>
2025-03-02 09:16:18 -0700

    chore: adding scores to db

--- main/src/app/api/addScore/route.ts
+++ main/src/app/api/addScore/route.ts
@@ -2,25 +2,25 @@ import { NextResponse } from "next/server";
 import clientPromise from "@/lib/mongodbClientPromise";
 import { ObjectId } from "mongodb";
 
-interface Occasion {
-  title: string;
-  date: string;
-  description?: string;
-}
-
 interface User {
   _id: ObjectId;
-  occasions?: Occasion[];
-  scores?: number[];
+  scores: number[];
 }
 
 export async function POST(req: Request) {
   try {
-    const { userId, occasionData } = await req.json();
+    const { userId, score } = await req.json();
 
-    if (!userId || !occasionData) {
+    if (!userId || score === undefined) {
       return NextResponse.json(
-        { error: "Missing userId or occasion data" },
+        { error: "Missing userId or score" },
+        { status: 400 },
+      );
+    }
+
+    if (typeof score !== "number") {
+      return NextResponse.json(
+        { error: "Score must be a number" },
         { status: 400 },
       );
     }
@@ -28,33 +28,33 @@ export async function POST(req: Request) {
     const client = await clientPromise;
     const db = client.db("skybridge-cluster");
     const users = db.collection<User>("users");
-
     const objectId = new ObjectId(userId);
 
     const updatedUser = await users.updateOne(
       { _id: objectId },
-      { $push: { occasions: occasionData as Occasion } },
+      {
+        $push: { scores: score },
+      },
       { upsert: true },
     );
-    
 
-    if (!updatedUser.matchedCount) {
+    if (!updatedUser.matchedCount && !updatedUser.upsertedCount) {
       return NextResponse.json({ error: "User not found" }, { status: 404 });
     }
 
     if (!updatedUser.modifiedCount) {
       return NextResponse.json(
-        { error: "Failed to add occasion" },
+        { error: "Failed to add score" },
         { status: 500 },
       );
     }
 
     return NextResponse.json(
-      { success: true, message: "Occasion added successfully" },
+      { success: true, message: "Score added successfully" },
       { status: 200 },
     );
   } catch (error) {
-    console.error("Error adding occasion:", error);
+    console.error("Error adding score:", error);
     return NextResponse.json(
       { error: "Something went wrong" },
       { status: 500 },
