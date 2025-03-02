c8989414b137081a91ccef717473b726263531b4
Nirmal Khedkar <nirmalhk7@gmail.com>
2025-03-01 22:29:10 -0700

    statcomm
    Signed-off-by: Nirmal Khedkar <nirmalhk7@gmail.com>

---
+++ main/src/components/AuthProvider/AuthProvider.ts
@@ -0,0 +1,46 @@
+"use client"
+import { signOut, useSession } from 'next-auth/react';
+import { usePathname, redirect } from 'next/navigation';
+import React, { createContext, ReactNode, useContext, useEffect, useState } from 'react';
+
+interface AuthProviderProps {
+    children: ReactNode;
+}
+
+const AuthContext = createContext({});
+const AUTHZ = {
+    Fundraiser: ["/dashboard"],
+    Sponsorer: ["/dashboard"],
+    PUBLIC: ["/","/signin","/signup"]
+}
+
+const AuthProvider: React.FC<AuthProviderProps> = ({ children }) => {
+      const [username, setUsername]= useState(null);
+      const { data: session, status } = useSession();
+      const currentRoute = usePathname();
+
+    
+      useEffect(()=>{
+        if(status==='authenticated'){
+            console.log('authenticated',session,  currentRoute);
+            if(currentRoute in AUTHZ[session.user.role]){
+                setUsername(session.user.name);
+            }
+            else {
+                // redirect('/dashboard')
+            }
+        } else if(status==='unauthenticated' && !AUTHZ.PUBLIC.includes(currentRoute)) {
+          redirect('/signin')
+        }
+      },[session, status]);
+
+    return (
+        <AuthContext.Provider value={{}}>
+            {children}
+        </AuthContext.Provider>
+    );
+};
+
+export const useAuth = () => useContext(AuthContext);
+
+export default AuthProvider;
